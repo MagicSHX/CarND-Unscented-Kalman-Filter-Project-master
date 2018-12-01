b4ed390513a48c638c48fd6a0b73954ef692bd2e
MagicSHX <34002636+MagicSHX@users.noreply.github.com>
2018-12-01 17:33:43 +0800

    y

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -45,7 +45,7 @@ UKF::UKF() {
   std_a_ = 1.;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 1.;
+  std_yawdd_ = 0.3;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
